93d7f54a8bcf2c04992ed2b9adc5b1b70e049525
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-12-12 15:01:35 +0200

    move files extesions type to hashset

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use crate::{Unit, UnitKind};
+use crate::{Unit, Units};
 use files_box::{ls, FilesBox};
 use leptos::{ev, prelude::*};
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
@@ -43,57 +43,6 @@ struct GlobalState {
     units_refetch_tick: bool,
 }
 
-fn retype_units(units: &mut Vec<Unit>) {
-    const VIDEO_X: [&str; 38] = [
-        "webm", "mkv", "flv", "vob", "ogv", "ogg", "rrc", "gifv", "mng", "mov", "avi", "qt", "wmv",
-        "yuv", "rm", "asf", "amv", "mp4", "m4p", "m4v", "mpg", "mp2", "mpeg", "mpe", "mpv", "m4v",
-        "svi", "3gp", "3g2", "mxf", "roq", "nsv", "flv", "f4v", "f4p", "f4a", "f4b", "mod",
-    ];
-
-    const AUDIO_X: [&str; 20] = [
-        "wav", "mp3", "aiff", "raw", "flac", "alac", "ape", "wv", "tta", "aac", "m4a", "ogg",
-        "opus", "wma", "au", "gsm", "amr", "ra", "mmf", "cda",
-    ];
-    units.iter_mut().for_each(|unit| {
-        if unit.kind != UnitKind::File {
-            return;
-        }
-        if let Some(x) = unit.path.extension().and_then(|x| x.to_str()) {
-            if VIDEO_X.contains(&x) {
-                unit.kind = UnitKind::Video;
-            } else if AUDIO_X.contains(&x) {
-                unit.kind = UnitKind::Audio;
-            }
-        };
-    });
-}
-
-fn sort_units(units: Vec<Unit>) -> Vec<Unit> {
-    let (mut directories, mut files, mut videos, mut audios) =
-        (Vec::new(), Vec::new(), Vec::new(), Vec::new());
-
-    for unit in units.into_iter() {
-        let target = match unit.kind {
-            UnitKind::Dirctory => &mut directories,
-            UnitKind::Video => &mut videos,
-            UnitKind::Audio => &mut audios,
-            UnitKind::File => &mut files,
-        };
-        target.push(unit);
-    }
-
-    [&mut directories, &mut videos, &mut audios, &mut files]
-        .iter_mut()
-        .for_each(|xs| xs.sort_by_key(|x| x.name()));
-
-    directories
-        .into_iter()
-        .chain(videos)
-        .chain(audios)
-        .chain(files)
-        .collect()
-}
-
 #[component]
 pub fn App() -> impl IntoView {
     let store = Store::new(GlobalState::default());
@@ -104,8 +53,8 @@ pub fn App() -> impl IntoView {
 
     Effect::new(move || {
         if let Some(mut xs) = ls_result.get().transpose().ok().flatten() {
-            retype_units(&mut xs);
-            *store.units().write() = sort_units(xs);
+            xs.retype();
+            *store.units().write() = xs.resort();
         };
     });
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use leptos::prelude::document;
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+use std::{cell::LazyCell, collections::HashSet, path::PathBuf};
 use wasm_bindgen::JsCast;
 
 pub mod app;
@@ -41,6 +41,70 @@ impl Unit {
             .unwrap()
             .unchecked_into::<web_sys::HtmlAnchorElement>()
             .click();
+    }
+}
+trait Units {
+    fn retype(&mut self);
+    fn resort(self) -> Self;
+}
+
+//TODO : try make this happen at compile time
+const VIDEO_X: LazyCell<HashSet<&str>> = LazyCell::new(|| {
+    HashSet::from([
+        "webm", "mkv", "flv", "vob", "ogv", "ogg", "rrc", "gifv", "mng", "mov", "avi", "qt", "wmv",
+        "yuv", "rm", "asf", "amv", "mp4", "m4p", "m4v", "mpg", "mp2", "mpeg", "mpe", "mpv", "m4v",
+        "svi", "3gp", "3g2", "mxf", "roq", "nsv", "flv", "f4v", "f4p", "f4a", "f4b", "mod",
+    ])
+});
+
+//TODO : try make this happen at compile time
+const AUDIO_X: LazyCell<HashSet<&str>> = LazyCell::new(|| {
+    HashSet::from([
+        "wav", "mp3", "aiff", "raw", "flac", "alac", "ape", "wv", "tta", "aac", "m4a", "ogg",
+        "opus", "wma", "au", "gsm", "amr", "ra", "mmf", "cda",
+    ])
+});
+
+impl Units for Vec<Unit> {
+    fn retype(&mut self) {
+        self.iter_mut().for_each(|unit| {
+            if unit.kind != UnitKind::File {
+                return;
+            }
+            if let Some(x) = unit.path.extension().and_then(|x| x.to_str()) {
+                if VIDEO_X.contains(&x) {
+                    unit.kind = UnitKind::Video;
+                } else if AUDIO_X.contains(&x) {
+                    unit.kind = UnitKind::Audio;
+                }
+            };
+        });
+    }
+
+    fn resort(self) -> Self {
+        let (mut directories, mut files, mut videos, mut audios) =
+            (Vec::new(), Vec::new(), Vec::new(), Vec::new());
+
+        for unit in self.into_iter() {
+            let target = match unit.kind {
+                UnitKind::Dirctory => &mut directories,
+                UnitKind::Video => &mut videos,
+                UnitKind::Audio => &mut audios,
+                UnitKind::File => &mut files,
+            };
+            target.push(unit);
+        }
+
+        [&mut directories, &mut videos, &mut audios, &mut files]
+            .iter_mut()
+            .for_each(|xs| xs.sort_by_key(|x| x.name()));
+
+        directories
+            .into_iter()
+            .chain(videos)
+            .chain(audios)
+            .chain(files)
+            .collect()
     }
 }
 
