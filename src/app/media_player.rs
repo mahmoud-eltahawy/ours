6cf6c19d095ffccf9d14e78592387e14ca7f8272
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-12-31 22:27:50 +0200

    floating media player

--- src/app/media_player.rs
+++ src/app/media_player.rs
@@ -1,4 +1,7 @@
-use leptos::{either::Either, prelude::*};
+use leptos::{either::Either, html::Div, prelude::*};
+use leptos_use::{
+    core::Position, use_draggable_with_options, use_window, UseDraggableOptions, UseDraggableReturn,
+};
 use reactive_stores::Store;
 
 use crate::{Unit, UnitKind};
@@ -9,14 +12,39 @@ use super::{GlobalState, GlobalStateStoreFields};
 pub fn MediaPlayer() -> impl IntoView {
     let store: Store<GlobalState> = use_context().unwrap();
     let media_play = store.media_play();
+    let el = NodeRef::<Div>::new();
+
+    let inner_height = use_window()
+        .as_ref()
+        .map(|w| w.inner_height().unwrap().as_f64().unwrap())
+        .unwrap_or(0.0);
+
+    let inner_width = use_window()
+        .as_ref()
+        .map(|w| w.inner_width().unwrap().as_f64().unwrap())
+        .unwrap_or(0.0);
+
+    let UseDraggableReturn { style, .. } = use_draggable_with_options(
+        el,
+        UseDraggableOptions::default()
+            .initial_value(Position {
+                x: inner_width / 4.4,
+                y: inner_height / 3.3,
+            })
+            .prevent_default(true),
+    );
 
     move || {
         media_play.get().map(|unit| {
             view! {
-                <section class="fixed bottom-2 right-2 bg-white text-2xl w-[60%] rounded-lg border-2 border-black p-2">
+                <div
+                    node_ref=el
+                    class="fixed bg-white rounded-lg text-2xl w-[70%] px-4 py-2 border border-gray-400/30 shadow hover:shadow-lg select-none cursor-move z-24"
+                    style=style
+                >
                     <Bar name=unit.name() />
                     <Player unit />
-                </section>
+                </div>
             }
         })
     }
@@ -25,11 +53,9 @@ pub fn MediaPlayer() -> impl IntoView {
 #[component]
 fn Bar(name: String) -> impl IntoView {
     view! {
-        <div class="relative">
-            <span class="absolute left-0 top-0 z-10 mr-10 text-nowrap">{name}</span>
-            <span class="absolute right-0 top-0 z-20">
-                <CloseButton />
-            </span>
+        <div class="flow-root">
+            <span class="w-[80%] float-left mr-10 truncate">{name}</span>
+            <CloseButton />
         </div>
     }
 }
@@ -46,7 +72,7 @@ fn CloseButton() -> impl IntoView {
     view! {
         <button
             on:click=close
-            class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+            class="float-right bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
         >
             <svg
                 class="h-6 w-6"
@@ -73,13 +99,13 @@ fn Player(unit: Unit) -> impl IntoView {
     let src = format!("/download/{}", unit.path.to_str().unwrap());
     match unit.kind {
         UnitKind::Video => Either::Left(view! {
-            <video class="h-full w-full rounded-lg" autoplay controls>
+            <video class="h-full w-full rounded-lg cursor-default" autoplay controls>
                 <source src=src />
                 "Your browser does not support the video tag."
             </video>
         }),
         UnitKind::Audio => Either::Right(view! {
-            <audio class="h-full w-full rounded-lg mt-10" autoplay controls>
+            <audio class="h-full w-full rounded-lg mt-10 cursor-default" autoplay controls>
                 <source src=src />
                 "Your browser does not support the audio tag."
             </audio>
