57ee54abd6b9f2b677f86f27123f3ea31cd5597d
mahmoud-eltahawy <gemmy9300@gmail.com>
2025-11-04 13:12:09 +0200

    refactor icons to be more safe

--- assets/src/lib.rs
+++ assets/src/lib.rs
@@ -1,44 +1,26 @@
-macro_rules! include_svg {
-    ($name:literal) => {
-        include_bytes!(concat!("../static/", $name, ".svg"))
-    };
-}
-
 pub const FAVICON: &[u8] = include_bytes!("../static/favicon.ico.gz");
 pub const TAILWINDJS: &[u8] = include_bytes!("../static/tailwind.js.gz");
 pub const HTMXJS: &[u8] = include_bytes!("../static/htmx.js.gz");
 
-//WARNING : this implementation requires same sorting on ICONS assignment and IconName declration
+macro_rules! build_icons_defs {
+    ($($name:ident);*) => {
+        #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+        pub enum IconName {
+            $(
+                $name,
+            )*
+        }
 
-pub const ICONS: [&[u8]; 11] = [
-    include_svg!("folder"),
-    include_svg!("file"),
-    include_svg!("video"),
-    include_svg!("audio"),
-    include_svg!("select"),
-    include_svg!("close"),
-    include_svg!("expand"),
-    include_svg!("collapse"),
-    include_svg!("download"),
-    include_svg!("home"),
-    include_svg!("upload"),
-];
+        pub const ICONS: [&[u8]; 11] = [
+            $(
+                include_bytes!(concat!("../static/", stringify!($name), ".svg")),
+            )*
+        ];
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Default)]
-pub enum IconName {
-    Folder,
-    File,
-    Video,
-    Audio,
-    Select,
-    Close,
-    Expand,
-    Collapse,
-    Download,
-    #[default]
-    Home,
-    Upload,
+    };
 }
+
+build_icons_defs!(Folder; File; Video; Audio; Select; Close; Expand; Collapse; Download; Home; Upload);
 
 impl From<u8> for IconName {
     fn from(value: u8) -> Self {
